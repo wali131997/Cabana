ba943de9913ee03bc78851b57199c4b8bd3b9e18
wali <wali131997@users.noreply.github.com>
2021-07-29 14:32:17 +0500

    w
    This reverts commit 9adcff63d9c45d6291da1f88e79e99d4c462a222.

--- resources/js/components/Redux/Reducer.js
+++ resources/js/components/Redux/Reducer.js
@@ -1,13 +1,15 @@
 
 const current_state = {
     promotion:[],
+    country:''
 }
 const reducer = (state = current_state,action) =>{
     if(action.type == 'CHANGE_PROMOTION'){
-        console.log(action.payload)
+        console.log(action.payload.promotion)
         return {
             ...state,
-            promotion:action.payload
+            promotion:action.payload.promotion,
+            country:action.payload.country
         }
     }
     return state;
