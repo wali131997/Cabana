4c3cd2cf7918bd37e9db1a6e9bf9e72c2e6abd1d
wali131997 <70524307+wali131997@users.noreply.github.com>
2022-10-28 04:05:55 +0500

    w

--- resources/js/components/pages/AccountTypesPakistan/components/CompareAccountStandardM.js
+++ resources/js/components/pages/AccountTypesPakistan/components/CompareAccountStandardM.js
@@ -188,37 +188,37 @@ class CompareAccountStandardM extends Component {
               </h4>
 
 
-              <h4 className="title d-inine "><span className="opacityZero">CURRENCY*</span></h4>
+              <h4 className="title d-inine "><span className="">CURRENCY*</span></h4>
             <h2 className="compareAccountNorMobText mt-3">USD</h2>
-              <h4 className="title d-inine "><span className="opacityZero">Spread</span></h4>
+              <h4 className="title d-inine "><span className="">Spread</span></h4>
               <p className="compareAccountNorMobText">From 0.6</p>
-              <h4 className="title d-inine "><span className="opacityZero">Commission Markup</span></h4>
+              <h4 className="title d-inine "><span className="">Commission Markup</span></h4>
               <h2 className="compareAccountNorMobText"> Nil</h2>
-              <h4 className="title d-inine "><span className="opacityZero">Minimum Deposit</span></h4>
+              <h4 className="title d-inine "><span className="">Minimum Deposit</span></h4>
               <h2 className="compareAccountNorMobText">$100 </h2>
-              <h4 className="title d-inine "><span className="opacityZero">LEVERAGE*</span></h4>
+              <h4 className="title d-inine "><span className="">LEVERAGE*</span></h4>
               <h2 className="compareAccountNorMobText"> Up to 1:500 for currencies</h2>
-              <h4 className="title d-inine "><span className="opacityZero">Instruments</span></h4>
+              <h4 className="title d-inine "><span className="">Instruments</span></h4>
              <h2 className="compareAccountNorMobText">  Majors, Minors, Spot Metals - 2, Spot Energy - 2, US30, GER30</h2>
-             <h4 className="title d-inine "><span className="opacityZero">SWAP</span></h4>
+             <h4 className="title d-inine "><span className="">SWAP</span></h4>
              <h2 className="compareAccountNorMobText"> <p className="text">Swap Free</p></h2>
-             <h4 className="title d-inine "><span className="opacityZero">OVERNIGHT COMMISSIONS</span></h4>
+             <h4 className="title d-inine "><span className="">OVERNIGHT COMMISSIONS</span></h4>
              <h2 className="compareAccountNorMobText"> <p className="text">No Commision</p></h2>
-             <h4 className="title d-inine "><span className="opacityZero">PRECISION</span></h4>
+             <h4 className="title d-inine "><span className="">PRECISION</span></h4>
              <h2 className="compareAccountNorMobText"> 5 decimals for FX (3
                 on JPY pairs), <br/> Spot Metals: 2 decimals for XAUUSD and 3 decimals
                 for XAGUSD</h2>
-             <h4 className="title d-inine "><span className="opacityZero">MARGIN CALL/STOP OUT LEVEL</span></h4>
+             <h4 className="title d-inine "><span className="">MARGIN CALL/STOP OUT LEVEL</span></h4>
              <h2 className="compareAccountNorMobText"> 50% / 30%</h2>
-             <h4 className="title d-inine "><span className="opacityZero">HeDGING</span></h4>
+             <h4 className="title d-inine "><span className="">HeDGING</span></h4>
              <h2 className="compareAccountNorMobText"> ✔</h2>
-             <h4 className="title d-inine "><span className="opacityZero">SCALPING</span></h4>
+             <h4 className="title d-inine "><span className="">SCALPING</span></h4>
              <h2 className="compareAccountNorMobText"> ✔</h2>
-             <h4 className="title d-inine "><span className="opacityZero">EXPERT ADVISOR</span></h4>
+             <h4 className="title d-inine "><span className="">EXPERT ADVISOR</span></h4>
              <h2 className="compareAccountNorMobText"> ✔</h2>
-             <h4 className="title d-inine "><span className="opacityZero">EXECUTION TIME</span></h4>
+             <h4 className="title d-inine "><span className="">EXECUTION TIME</span></h4>
              <h2 className="compareAccountNorMobText"> Execution in under 0.1 second</h2>
-             <h4 className="title d-inine "><span className="opacityZero">MAXIMUM ORDERS</span></h4>
+             <h4 className="title d-inine "><span className="">MAXIMUM ORDERS</span></h4>
              <h2 className="compareAccountNorMobText"> 500</h2>
 
               <button onClick={()=>{ window.open('https://secure.cabanacapitals.com/register/','_self')}} className="btnPrimaryPink2 pl-2 pr-2 mr-0 mt-4 mb-5">
