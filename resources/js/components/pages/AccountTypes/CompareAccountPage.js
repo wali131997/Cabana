3d01a4733db1c7ac5a72a0cc9af7319676f50cf4
wali131997 <70524307+wali131997@users.noreply.github.com>
2024-01-28 17:41:58 +0500

    w

--- resources/js/components/pages/AccountTypes/CompareAccountPage.js
+++ resources/js/components/pages/AccountTypes/CompareAccountPage.js
@@ -6,9 +6,10 @@ import CompareAccountStandard from "./components/CampareAccountStandard";
 import CompareAccountEcn from "./components/CompareAccountEcn";
 import CompareAccountStandardM from "./components/CompareAccountStandardM";
 import CompareAccountEcnM from "./components/CompareAccountEcnM"
-export default class CompareAccountPage extends Component {
-  constructor() {
-    super();
+import { connect } from "react-redux";
+class CompareAccountPage extends Component {
+  constructor(props) {
+    super(props);
     // state
     this.state = {
       active: true,
@@ -29,44 +30,66 @@ export default class CompareAccountPage extends Component {
         <HeroSection title="COMPARE ACCOUNTS"
         bg="accounttypes.jpg"
         />
-        <div className="container">
-          <div className="row compareAccountRow">
-            <CompareAccoountHeader
-              active={active}
-              handleActiveFun={this.handleActiveFun.bind(this)}
-            />
-          </div>
+        {
+            this.props.country ?
+                <div className="container">
+                <div className="row compareAccountRow">
+                    <CompareAccoountHeader
+                    active={active}
+                    handleActiveFun={this.handleActiveFun.bind(this)}
+                    />
+                </div>
 
-          {active===true?
+                {active===true?
 
-          <><div
-            className="row compareAccountRow d-none d-md-flex"
-          >
-            <CompareAccountStandard />
-          </div>
+                <><div
+                    className="row compareAccountRow d-none d-md-flex"
+                >
+                    <CompareAccountStandard />
+                </div>
 
 
-          <div className="row compareAccountRowM d-md-none">
-            <CompareAccountStandardM/></div> </>:<>
+                <div className="row compareAccountRowM d-md-none">
+                    <CompareAccountStandardM/></div> </>:<>
 
-             <div
-            className="row compareAccountRow d-none d-md-flex"
-          >
+                    <div
+                    className="row compareAccountRow d-none d-md-flex"
+                >
 
-            <CompareAccountEcn />
-          </div>
-          <div className="row compareAccountRowM d-md-none">
-            <CompareAccountEcnM/>
-            </div>
+                    <CompareAccountEcn />
+                </div>
+                <div className="row compareAccountRowM d-md-none">
+                    <CompareAccountEcnM/>
+                    </div>
 
-          </>}
+                </>}
 
-            <div className="text-center">
-                <p>*Refer to Client Agreement</p>
-            </div>
+                    <div className="text-center">
+                        <p>*Refer to Client Agreement</p>
+                    </div>
 
-        </div>
+                </div>
+                :
+                <div className="container ">
+
+
+                    <div className="text-center mt-5 pt-5 mb-5 pb-5">
+
+                    <div class="spinner-border text-dark" role="status">
+                    <span class="sr-only">Loading...</span>
+                    </div>
+                    <h6 className="text-center mt-2">Please wait, Account types are being loaded</h6>
+                    </div>
+                </div>
+        }
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) =>{
+    return{
+        country:state.country
+    }
+}
+export default connect(mapStateToProps)(CompareAccountPage);
--- resources/js/components/pages/AccountTypes/components/CompareAccountEcnM.js
+++ resources/js/components/pages/AccountTypes/components/CompareAccountEcnM.js
@@ -30,7 +30,6 @@ class CompareAccountEcnM extends Component {
         this.setState({
             accountsState: filteredAccounts
         })
-
     }
     render() {
         return (
