664a747ace8c81e93977ef080173de8ea74e7767
waleedse <70524307+waleedse@users.noreply.github.com>
2024-04-19 23:39:29 +0500

    w

--- resources/js/components/HeaderComp/PAMMsDropDown.js
+++ resources/js/components/HeaderComp/PAMMsDropDown.js
@@ -1,52 +1,52 @@
 import React, { Component } from "react";
 import { a } from "react-router-dom";
 export default class PAMMsDropDown extends Component {
-  render() {
-    return (
-      <div>
-        <div className="headerDropDown">
-          <div
-            className="headerDropDownItemDiv offset-md-3"
-            onClick={this.props.dropDownHandler}
-          >
-            <h5 className="headerDropDownTitle">Copy Trading</h5>
-            <a href="https://social.cabanacapitals.com/portal/login">
-              <div className="headerDropDownItem">Social Login</div>
-            </a>
-            <a href="/social-trading">
-              <div className="headerDropDownItem">Copy Trading</div>
-            </a>
-            {/* <a href="/PAMM">
+    render() {
+        return (
+            <div>
+                <div className="headerDropDown">
+                    <div
+                        className="headerDropDownItemDiv offset-md-3"
+                        onClick={this.props.dropDownHandler}
+                    >
+                        <h5 className="headerDropDownTitle">Copy Trading</h5>
+                        <a href="https://social.cabanacapitals.com/portal/login">
+                            <div className="headerDropDownItem">Cpoy trading Login</div>
+                        </a>
+                        <a href="/social-trading">
+                            <div className="headerDropDownItem">Copy Trading</div>
+                        </a>
+                        {/* <a href="/PAMM">
               <div className="headerDropDownItem">PAMM Manager</div>
             </a> */}
-            {/* <a href="/PAMM">
+                        {/* <a href="/PAMM">
               <div className="headerDropDownItem">Copy Trading</div>
             </a> */}
-          </div>
-          <div
-            className="headerDropDownItemDiv "
-            onClick={this.props.dropDownHandler}
-          >
+                    </div>
+                    <div
+                        className="headerDropDownItemDiv "
+                        onClick={this.props.dropDownHandler}
+                    >
 
-            <h5 className="headerDropDownTitle mt-4">PAMM MANAGER</h5>
-            <a href="/PAMM">
-              <div className="headerDropDownItem">PAMM Manager</div>
-            </a>
-            <a href="/strategy-manager" >
-              <div className="headerDropDownItem">PAMM Strategy Manager</div>
-            </a>
-            <a href="/PAMM" className="opacityZero">
-              <div className="headerDropDownItem">PAMM Manager</div>
-            </a>
-          </div>
-          <div className="headerDropDownImgDiv">
-            <img
-              className="headerDropDownImg"
-              src="/assets/images/pammDrop.svg"
-            ></img>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                        <h5 className="headerDropDownTitle mt-4">PAMM MANAGER</h5>
+                        <a href="/PAMM">
+                            <div className="headerDropDownItem">PAMM Manager</div>
+                        </a>
+                        <a href="/strategy-manager" >
+                            <div className="headerDropDownItem">PAMM Strategy Manager</div>
+                        </a>
+                        <a href="/PAMM" className="opacityZero">
+                            <div className="headerDropDownItem">PAMM Manager</div>
+                        </a>
+                    </div>
+                    <div className="headerDropDownImgDiv">
+                        <img
+                            className="headerDropDownImg"
+                            src="/assets/images/pammDrop.svg"
+                        ></img>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
