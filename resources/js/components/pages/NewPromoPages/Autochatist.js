4d93a3b887a1cd104f05849d1f100332bad5103a
waleedse <70524307+waleedse@users.noreply.github.com>
2024-08-19 01:23:56 +0500

    w

--- resources/js/components/pages/NewPromoPages/Autochatist.js
+++ resources/js/components/pages/NewPromoPages/Autochatist.js
@@ -15,7 +15,7 @@ const Autochartist = () => {
                     </div>
                 </div>
             </div>
-
+            <br></br>
             <div className='container mb-5'>
 
                 <div className='row mt-5'>
@@ -26,14 +26,13 @@ const Autochartist = () => {
                         <h4 className='text-bold atitle text-left px-0 pb-2' style={{ color: '#9b005f', fontSize: '38px' }}>
                             What is Autochartist? </h4>
                         <h5 className='font-weight-light title text-left mt-2' >
-                            Renowned Forex trading tool Autochartist provides automatic market analysis based on sophisticated algorithms that search for possible trading opportunities based on analysis of past and present market trends.
-                            <br></br><br></br>
-                            Autochartist is an excellent tool that gives Cabana Capitals clients a competitive advantage in the market and fast, actionable information to improve their trading tactics. It does this by identifying high-probability trade setups, providing real-time trading signals, and doing in-depth market volatility analysis.
-
+                            Autochartist is a premium trading suite with tools that automate market analysis for forex, stocks, and crypto traders. It uses sophisticated algorithms to identify high-probability trade setups and provides real-time trading signals by analyzing past and present market trends. These essential tools save traders time and offer fast, actionable insights, giving Cabana Capitals' clients a competitive advantage in dynamic markets.
                         </h5>
 
                     </div>
                 </div>
+                <br></br>
+                <br></br>
                 <div>
                     <h4 className='text-bold atitle text-left px-0 pb-1' style={{ color: '#9b005f', fontSize: '22px' }}>
 
@@ -47,6 +46,8 @@ const Autochartist = () => {
                         <br></br>Market Report email service, which is partnered with Autochartist. These reports serve as your
                         <br></br>personal market guide, providing more than just summaries. They include:
                     </h5>
+                    <br></br>
+
                     <div className='row mt-3'>
                         <div className='col-md-4 text-center'>
                             <img style={{ width: '65px' }} src='/assets/images/autochartist/stars.png'></img>
@@ -71,13 +72,15 @@ const Autochartist = () => {
                             </h5>
                         </div>
                     </div>
-                    <div className='text-center'>
+                    {/* <div className='text-center'>
 
                         <button className='btn  btn-dark btn-lg  mt-4 p-3' style={{ backgroundColor: '#000', borderRadius: '7px' }} onClick={() => { window.open('https://secure.cabanacapitals.com/register/', '_self') }}>Create an Account - It’s Free!</button>
-                    </div>
+                    </div> */}
 
                 </div>
-                <h4 className='text-bold atitle text-center px-0 pb-0 mt-4' style={{ color: '#9b005f', fontSize: '22px' }}>
+                <br></br>
+
+                {/* <h4 className='text-bold atitle text-center px-0 pb-0 mt-4' style={{ color: '#9b005f', fontSize: '22px' }}>
 
                     ACCESS THE POWER OF AUTOCHARTIST
                 </h4>
@@ -94,28 +97,40 @@ const Autochartist = () => {
                 <div className='text-center'>
 
                     <button className='btn  btn-dark btn-lg  mt-2 px-5' style={{ backgroundColor: '#A2237E', borderRadius: '7px' }} onClick={() => { window.open('https://secure.cabanacapitals.com/register/', '_self') }}>Login to Portal</button>
+                </div> */}
+
+                <div className='container'>
+                    <p className='mb-0 pb-0'>Unpack weekly Success  </p>
+                    <h2>Best 5 Trades Emails</h2>
+                    <h5 className='font-weight-light title text-left mt-2' style={{ fontSize: '16px' }} >
+                        Subscribe to our “Best 5 Trades” Email and start Mondays informed with a review of the previous week's most successful trades. This essential roundup showcases the depth and quality of market analysis, helping you refine your strategies without additional analysis.
+
+                    </h5>
+                    <div className='container px-3'>
+                        - Receive weekly updates on the most successful trades of the last week.
+                        <br></br>- Apply insights from proven strategies to improve your trading knowledge.
+                        <br></br>- Benefit from an improved understanding of market movements and see what you missed.
+                    </div>
+                    <div className='text-center'>
+                        <button className='btn btn-lg  mt-2 px-5 mt-3 text-light' style={{ backgroundColor: '#C82884', borderRadius: '0px' }} onClick={() => { window.open('https://reports.autochartist.com/signup/feeds/113?bid=897&user=email&demo=0&locale=en&expire=1759183200&token=6a2571973ee44bcef03832d338fd4263', '_self') }}>
+                            Subscribe
+                        </button>
+                    </div>
                 </div>
-
-
                 <div className='row mt-5'>
                     <div className='col-md-6'>
-                        <img style={{ width: '100%' }} className='pt-5' src='/assets/images/autochartist/Laptop2.png'></img>
+                        <img style={{ width: '100%' }} className='pt-5' src='/assets/images/autochartist/Laptop1.png'></img>
                     </div>
                     <div className='col-md-6'>
                         <h4 className='text-bold atitle text-left px-0 pb-0' style={{ color: '#9b005f', fontSize: '14px' }}>
-                            ENHANCE YOUR METATRADER
+                            Navigate Market Movements
                         </h4>
                         <h4 className='text-bold atitle text-left px-0 pb-2 pt-0' style={{ color: '#000', fontSize: '38px' }}>
-                            Autochartist LaunchPad </h4>
-                        <h5 className='font-weight-light title text-left mt-2' style={{ fontSize: '16px' }} >
-                            With the Autochartist LaunchPad, which is made for smooth interaction with both MT4 and MT5 platforms, you may improve your trading approach. You can directly use Autochartist's extensive market analysis tools with this cutting-edge application. Discover LaunchPad's power with:
+                            Autochartist Volatility Analysis</h4>
+                        <h5 className='font-weight-light title text-left text-justify-center mt-2' style={{ fontSize: '16px' }} >
 
-                            <br></br><br></br>
-                            <strong>MT4 and MT5 Compatibility:</strong> For a simplified trading experience, just integrate with your favorite MetaTrader platform
-                            <br></br><br></br>
-                            <strong>Effective Market Analysis:</strong> Using MT4 or MT5, quickly and thoroughly analyze Forex markets to find possibilities for real-time trading
-                            <br></br><br></br>
-                            <strong>Personalized Alerts:</strong> Improve your trading decisions by setting up personalized alerts for important market events and pattern developments.
+                            Volatility Analysis offers detailed forecasts of trading ranges for all instruments over the upcoming 1 hour, 4 hours, and 24 hours. This tool equips traders with a clear understanding of anticipated price fluctuations, facilitating effective navigation of market volatility. Its visual displays and comprehensive data empower even novice traders to integrate these insights into their trading strategies. Popular with traders of all levels, Volatility Analysis aids in making well-informed decisions in the ever-changing market landscape.
+
                         </h5>
 
                     </div>
@@ -129,16 +144,16 @@ const Autochartist = () => {
                         <h4 className='text-bold atitle text-left px-0 pb-2 pt-0' style={{ color: '#000', fontSize: '38px' }}>
                             Technical Analysis</h4>
                         <h5 className='font-weight-light title text-left mt-2' style={{ fontSize: '16px' }} >
-                            Make use of the sophisticated technical analysis tools and indicators available in Autochartist. In order to help you remain ahead in the volatile Forex market, this section offers real-time market scanning that can spot patterns and trends as they appear and present you with personalized notifications.
+                            This dynamic trading tool delivers near real-time, precise insights into thousands of trade setups across multiple asset classes, enabling rapid identification and response to market trends and reversals with unmatched speed and accuracy.
 
-                            <br></br> <br></br> - Sophisticated indications and tools for technical analysis.
-                            <br></br>  - Searching the Forex markets in real time for trends and patterns.
-                            <br></br>  - Alerts that can be customized for new and finished patterns.
+                            <br></br> <br></br> <strong style={{ fontWeight: 800 }}>- Automatically detects chart patterns, key levels, and Fibonacci patterns.</strong>
+                            <br></br> <strong style={{ fontWeight: 800 }}>- Real-time insights of "breakout" and "emerging" trade setups.</strong>
+                            <br></br> <strong style={{ fontWeight: 800 }}>- Covers various financial instruments to complement any trading strategy.</strong>
                         </h5>
 
                     </div>
                     <div className='col-md-6'>
-                        <img style={{ width: '100%' }} className='pt-5' src='/assets/images/autochartist/Laptop1.png'></img>
+                        <img style={{ width: '100%' }} className='pt-5' src='/assets/images/autochartist/Laptop2.png'></img>
                     </div>
                 </div>
                 <div className='row mt-5'>
@@ -147,16 +162,17 @@ const Autochartist = () => {
                     </div>
                     <div className='col-md-6'>
                         <h4 className='text-bold atitle text-left px-0 pb-0' style={{ color: '#9b005f', fontSize: '14px' }}>
-                            TAILOR YOUR TRADING EXPERIENCE
+                            Explore Statistics and Historical Data
                         </h4>
                         <h4 className='text-bold atitle text-left px-0 pb-2 pt-0' style={{ color: '#000', fontSize: '38px' }}>
-                            Autochartist Favorites </h4>
+                            Autochartist Performance
+                            Stats</h4>
                         <h5 className='font-weight-light title text-left mt-2' style={{ fontSize: '16px' }} >
-                            Customize your trading strategy with Autochartist Favorites. This feature allows you to customize your choice of instruments and models, control your preferred settings and receive notifications, focusing on the business opportunities that matter most to you.
+                            everage the Autochartist Performance Statistics Tool to improve your trading strategy. This tool assesses the success rate of Autochartist forecasts over the last six months, offering traders valuable insights into the accuracy and reliability of historical pattern outcomes.
 
-                            <br></br><br></br> - Personal choice of instruments and patterns.
-                            <br></br>- Ability to track desired models and receive notifications.
-                            <br></br>- Better focus on key business opportunities.
+                            <br></br><br></br> <strong style={{ fontWeight: 800 }}>- Gain confidence with verified success rates.</strong>
+                            <br></br><strong style={{ fontWeight: 800 }}>- Enhance decision-making with historical pattern data.</strong>
+                            <br></br><strong style={{ fontWeight: 800 }}>- Build more thoughtful strategies using statistical insights</strong>
                         </h5>
 
                     </div>
@@ -166,21 +182,63 @@ const Autochartist = () => {
 
                     <div className='col-md-6'>
                         <h4 className='text-bold atitle text-left px-0 pb-0' style={{ color: '#9b005f', fontSize: '14px' }}>
-                            UP-TO-DATE FINANCIAL NEWS
+                            Tailor Your Trading Experience
                         </h4>
                         <h4 className='text-bold atitle text-left px-0 pb-2 pt-0' style={{ color: '#000', fontSize: '38px' }}>
-                            Market News</h4>
+                            Autochartist Favorites</h4>
                         <h5 className='font-weight-light title text-left mt-2' style={{ fontSize: '16px' }} >
-                            If you can narrow down your feed using the search function, you can see current financial news content. Stock news, Forex news and general news from several popular news providers, with trending news and the ability to search for specific asset news..
+                            Customize your trading strategy with Autochartist Favorites. Tailor your experience by choosing specific instruments and models, adjusting settings, and setting up notifications to focus only on the highest probability opportunities.
 
-                            <br></br><br></br>- Up-to-date news and analysis on the Forex market.
-                            <br></br>- An overview of how global events affect currency movements.
-                            <br></br>- Necessary to make informed decisions and develop strategy..
+                            <br></br><br></br> <strong style={{ fontWeight: 800 }}>- Personalized setting for instruments and patterns.</strong>
+                            <br></br> <strong style={{ fontWeight: 800 }}>- Ability to track desired models and receive notifications.</strong>
+                            <br></br> <strong style={{ fontWeight: 800 }}>- Better focus on significant trading opportunities.</strong>
                         </h5>
 
                     </div>
                     <div className='col-md-6'>
-                        <img style={{ width: '100%' }} className='pt-5' src='/assets/images/autochartist/Laptop1.png'></img>
+                        <img style={{ width: '100%' }} className='pt-5' src='/assets/images/autochartist/Laptop4.png'></img>
+                    </div>
+                </div>
+
+                <div className='row mt-5'>
+                    <div className='col-md-6'>
+                        <img style={{ width: '100%' }} className='pt-5' src='/assets/images/autochartist/Laptop5.png'></img>
+                    </div>
+                    <div className='col-md-6'>
+                        <h4 className='text-bold atitle text-left px-0 pb-0' style={{ color: '#9b005f', fontSize: '14px' }}>
+                            Explore Statistics and Historical Data
+                        </h4>
+                        <h4 className='text-bold atitle text-left px-0 pb-2 pt-0' style={{ color: '#000', fontSize: '38px' }}>
+                            Autochartist News
+                            Sentiment
+                        </h4>
+                        <h5 className='font-weight-light title text-left mt-2' style={{ fontSize: '16px' }} >
+
+                            Harness the power of Autochartist News Sentiment, an intuitive tool that analyzes financial news from top sources. It offers a clear view of sentiment trends across various time frames and provides a visual sentiment score for major currencies and US stocks.
+
+                            <br></br><br></br> <strong style={{ fontWeight: 800 }}>- Understand complex news sentiment trends.</strong>
+                            <br></br><strong style={{ fontWeight: 800 }}>- Identify potential market movements quickly.</strong>
+                            <br></br><strong style={{ fontWeight: 800 }}>- Simplify searches with an intuitive interface.</strong>
+                        </h5>
+
+                    </div>
+                </div>
+
+                <div className='text-center mt-5'>
+                    <h3>Check out our <strong>social media links</strong></h3>
+                </div>
+                <div className='row'>
+                    <div className='col-md-3 p-2'>
+                        <img style={{ width: '100%' }} src='/assets/images/autochartist/1.jpg'></img>
+                    </div>
+                    <div className='col-md-3 p-2'>
+                        <img style={{ width: '100%' }} src='/assets/images/autochartist/2.jpg'></img>
+                    </div>
+                    <div className='col-md-3 p-2'>
+                        <img style={{ width: '100%' }} src='/assets/images/autochartist/3.jpg'></img>
+                    </div>
+                    <div className='col-md-3 p-2'>
+                        <img style={{ width: '100%' }} src='/assets/images/autochartist/4.jpg'></img>
                     </div>
                 </div>
             </div>
@@ -189,7 +247,7 @@ const Autochartist = () => {
 
 
 
-        </div>
+        </div >
     );
 };
 
