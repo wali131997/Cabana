c9085ada58129790f348f82b53a8778c72d3ad97
waleedse <70524307+waleedse@users.noreply.github.com>
2024-10-03 17:54:48 +0500

    w

--- resources/js/components/Footer.js
+++ resources/js/components/Footer.js
@@ -20,12 +20,12 @@ class Footer extends Component {
                             ></img>
                             <h3 className="footerTitle">Registered Office</h3>
                             <p className="footerText">
-                            Cabana Capitals Limited<br />
-Suite 305, Griffith Corporate Centre,<br /> PO Box 1510, Beachmont, Kingstown,<br /> Saint Vincent and the Grenadines
-<br /><br />Cabana Markets Limited<br />
+                                Cabana Capitals Limited<br />
+                                Suite 305, Griffith Corporate Centre,<br /> PO Box 1510, Beachmont, Kingstown,<br /> Saint Vincent and the Grenadines
+                                {/*<br /><br />Cabana Markets Limited<br />
                                 Office no. 212, Ground floor ,Block A,
-                                <br/>The junction Business Hub, <br/>
-                                Calebasses Branch Rd, Calebasses, Mauritius
+                                <br />The junction Business Hub, <br />
+        Calebasses Branch Rd, Calebasses, Mauritius */}
                             </p>
                             <div className="footerSocialBox">
                                 <div className="footerSocialCircle">
@@ -105,7 +105,7 @@ Suite 305, Griffith Corporate Centre,<br /> PO Box 1510, Beachmont, Kingstown,<b
                 </div>
                 <div className="row footerSecondRow">
                     {
-                         this.props.continent != 'Africa'?
+                        this.props.continent != 'Africa' ?
                             <p className="footerText footerText_justify">
                                 <span className="textBold">Legal: </span>
                                 Cabana Capitals Limited is incorporated in St. Vincent & the Grenadines as an International Broker Company
@@ -113,7 +113,7 @@ Suite 305, Griffith Corporate Centre,<br /> PO Box 1510, Beachmont, Kingstown,<b
                                 by International Business Companies (Amendment and Consolidation) Act, Chapter 149 of the Revised Laws of Saint
 
                                 Vincent and Grenadines, 2009, in particular but not exclusively all commercial, financial, lending, borrowing,
-                                 trading, service activities and the participation in other enterprises as well as href provide brokerage, training and managed account services in currencies, commodities, indexes, CFD's and leveraged financial instruments. Risk Warning: Trading Forex and CFDs involves significant risk and can result in the loss of your invested capital. You should not invest more than you can afford href lose and should ensure that you fully understand the risks involved. Trading leveraged products may not be suitable for all investors. Before trading, please take into consideration your level of experience, investment objectives and seek independent financial advice if necessary. It is the responsibility of the Client href ascertain whether he/she is permitted href use the services of the Cabana Capitals based on the legal requirements in his/her country of residence. Please read Cabana Capitals™ full Risk Disclosure. Regional Restrictions: Cabana Capitals does not provide services href residents of the EU Region, USA, Canada, Belgium, India and FATF Black Listed Countries. Find out more in the Account Opening section of our FAQs.
+                                trading, service activities and the participation in other enterprises as well as href provide brokerage, training and managed account services in currencies, commodities, indexes, CFD's and leveraged financial instruments. Risk Warning: Trading Forex and CFDs involves significant risk and can result in the loss of your invested capital. You should not invest more than you can afford href lose and should ensure that you fully understand the risks involved. Trading leveraged products may not be suitable for all investors. Before trading, please take into consideration your level of experience, investment objectives and seek independent financial advice if necessary. It is the responsibility of the Client href ascertain whether he/she is permitted href use the services of the Cabana Capitals based on the legal requirements in his/her country of residence. Please read Cabana Capitals™ full Risk Disclosure. Regional Restrictions: Cabana Capitals does not provide services href residents of the EU Region, USA, Canada, Belgium, India and FATF Black Listed Countries. Find out more in the Account Opening section of our FAQs.
                                 {/* Cabana (Markets) Limited is regulated by the financial services commission (FSC) of Republic of Mauritius, c
                                 ategory 1 Global Business No. GB22200748License | Company Reg. No 195270 GBC. */}
                             </p>
@@ -127,7 +127,7 @@ Suite 305, Griffith Corporate Centre,<br /> PO Box 1510, Beachmont, Kingstown,<b
                                 Regional Restrictions: Cabana Capitals does not provide services href residents of the EU Region, USA, Canada, Belgium, India and FATF Black Listed Countries. Find out more in the Account Opening section of our FAQs.
                                 {/* <br /> <br />Cabana Capitals brand is represented by the following entities and regulations: */}
                                 <br /><br />Cabana Capitals Ltd incorporate in St. Vincent & the Grenadines with company registration number 24185 IBC 2017.
-                                <br /><br /> Cabana Capitals is regulated in South Africa under the name of Gao Financial Services (Pty) Ltd and holds a Financial Service Provider (FSP) license number 44692 from the Financial Sector Conduct Authority (FSCA) in South Africa.
+                                {/* <br /><br /> Cabana Capitals is regulated in South Africa under the name of Gao Financial Services (Pty) Ltd and holds a Financial Service Provider (FSP) license number 44692 from the Financial Sector Conduct Authority (FSCA) in South Africa. */}
                             </p> :
                             <></>
                     }
@@ -140,15 +140,15 @@ Suite 305, Griffith Corporate Centre,<br /> PO Box 1510, Beachmont, Kingstown,<b
                             </p> :
                             <></>
                     }
-                    {
-                        this.props.country != 'India' &&   this.props.country != 'South Africa' && this.props.country != 'United Arab Emirates' ?
-                            <p className="footerText footerText_justify">
-                                Cabana capitals is regulated in Mauritius under the name of Cabana (Markets) Limited and is regulated by the Financial Services Commision of Mauritius (FSC) under the license number
-                                GB22200748. The FSC is the principal regulator of the financial services industry in Mauritius.
 
-                            </p> :
-                            <></>
-                    }
+                    this.props.country != 'India' &&   this.props.country != 'South Africa' && this.props.country != 'United Arab Emirates' ?
+                    <p className="footerText footerText_justify">
+                        Cabana capitals is regulated in Mauritius under the name of Cabana (Markets) Limited and is regulated by the Financial Services Commision of Mauritius (FSC) under the license number
+                        GB22200748. The FSC is the principal regulator of the financial services industry in Mauritius.
+
+                    </p> :
+                    <></>
+
                     {/* <p className="footerText">
             CCL FINTECH LIMITED is registered in the United Kindom, Company
             number 11501218. Registered address: 71-75 Shelton Street, London,
